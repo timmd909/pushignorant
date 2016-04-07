196eb72dca412ee0805e843092c4a93706e2ad6e
Tim Doerzbacher <tim@tim-doerzbacher.com>
2016-04-07 03:24:19 -0400

    Slowly adding KO functionality to submit picks

--- web/js/submit-picks.js
+++ web/js/submit-picks.js
@@ -3,21 +3,29 @@ require({
     paths: {
         'jquery': 'lib/jquery',
         'knockout': 'lib/knockout',
+        'lodash': 'lib/lodash'
     }
 }, [
     'knockout',
-    'jquery'
+    'jquery',
+    'lodash'
 ],
-function (ko, $) {
+function (ko, $, _) {
     var viewModel = {};
+
+    viewModel.loading = ko.observable(true);
+    viewModel.loadingFailed = ko.observable(false);
+    viewModel.leagues = ko.observableArray([]);
 
     $.ajax({
         url: 'json/teams',
-        success: function (a, b, c) {
-            debugger;
+        success: function (data) {
+            viewModel.loading(false);
+            viewModel.leagues(data.Leagues);
         },
-        failure: function (a, b, c) {
-            debugger;
+        error: function () {
+            viewModel.loading(false);
+            viewModel.loadingFailed(true);
         },
     })
 
